4f043ad2544a121aa0497e9ab9a30468f70d3613
Peter Burkholder <peter.burkholder@gsa.gov>
2021-02-26 17:40:40 -0500

    checkpoint in manifest and docker-setup

--- setup-docker-secrets.sh
+++ setup-docker-secrets.sh
@@ -9,17 +9,19 @@ fi
 
 if ! grep -qF "DATABASE_PASSWORD" .env; then
 	echo ".env does not contain DATABASE_PASSWORD, generating secret..."
-	echo "DATABASE_PASSWORD=$(openssl rand -hex 33)" >> .env
+	echo "DATABASE_PASSWORD=${DATABASE_PASSWORD:-$(openssl rand -hex 33)}" >> .env
 fi
 
 if [ -f .env-prod ]; then
 	echo ".env-prod already exists, if you would like to regenerate your secrets, please delete this file and re-run the script."
 else
 	echo ".env-prod does not exist, creating..."
-  read -p 'Enter JWT_EXPIRE_TIME ex. 1d or 25m: ' expire
-  cat >.env-prod - << EOF
-JWT_SECRET=$(openssl rand -hex 64)
-JWT_EXPIRE_TIME=$expire
+	if [ -z "$JWT_EXPIRE_TIME" ]; then
+		read -p 'Enter JWT_EXPIRE_TIME ex. 1d or 25m: ' JWT_EXPIRE_TIME
+	fi
+	cat >.env-prod - << EOF
+	JWT_SECRET=$(openssl rand -hex 64)
+	JWT_EXPIRE_TIME=$JWT_EXPIRE_TIME
 EOF
 fi
 
